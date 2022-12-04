a1536f5cfc4e700e60dca845f2b991ce6fd96b85
User <@kuksa>
2022-12-04 17:37:14 +0300

    final

--- Lab2A/src/bsu/rfe/java/group6/lab2/Kuksa/varA11/Formula.java
+++ Lab2A/src/bsu/rfe/java/group6/lab2/Kuksa/varA11/Formula.java
@@ -1,7 +1,7 @@
 package bsu.rfe.java.group6.lab2.Kuksa.varA11;
 
 public class Formula {
-    public static double sum = 10;
+    public static double sum = 0;
     public static double result;
     public static double numerator1;
     public static double denominator1;
